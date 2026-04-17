b716f7dad4a662d64831f42775d586cbb6db39b8
SaiMithunPunna <saimithunamangal@gmail.com>
2026-04-17 22:16:06 +0530

    handling input errors for expense

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
+from datetime import datetime
 def add_expense():
-    date=date(input())
-    category=input()
-    amount=float(input())
-    description=input()
+    try:
+        date = datetime.strptime(input("Enter date (YYYY-MM-DD): "), "%Y-%m-%d")
+        category=input("Enter the category : ")
+        amount=float(input("Enter the expense : "))
+        description=input("Enter description og ")
+    except ValueError:
+        print("Enter valid details.")
     expense={
         "date":date,
         "category":category,
         "amount":amount,
         "description":description
     }
+    
+
