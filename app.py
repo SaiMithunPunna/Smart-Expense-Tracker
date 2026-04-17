760510844b58e5e04d768f5db2de255ba440f05e
SaiMithunPunna <saimithunamangal@gmail.com>
2026-04-17 22:29:12 +0530

    created expenses file

--- app.py
+++ app.py
@@ -1,17 +1,38 @@
 from datetime import datetime
+import csv 
+import os
+
+file_name = "expenses.csv"
+
+
 def add_expense():
+
+    
+
+    #creating expenses file if not available
+    if not os.path.exists(file_name):
+        with open(file_name, mode='w', newline='') as file:
+            writer=csv.writer(file)
+            writer.writerow(["Date", "Category", "Amount", "Description"])
     try:
         date = datetime.strptime(input("Enter date (YYYY-MM-DD): "), "%Y-%m-%d")
         category=input("Enter the category : ")
         amount=float(input("Enter the expense : "))
         description=input("Enter description og ")
+
+        # adding data to the expenses file
+        with open(file_name, mode='a', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow([date, category, amount, description])
+        print("added new expense")
+
     except ValueError:
         print("Enter valid details.")
-    expense={
-        "date":date,
-        "category":category,
-        "amount":amount,
-        "description":description
-    }
+    return 
+
+add_expense()
     
+
+
+
 
