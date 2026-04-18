233dd270b3172d38104266e612aca7bc4888a615
SaiMithunPunna <saimithunamangal@gmail.com>
2026-04-18 15:26:44 +0530

    added view expenses function

--- app.py
+++ app.py
@@ -7,15 +7,13 @@ file_name = "expenses.csv"
 
 def add_expense():
 
-    
-
     #creating expenses file if not available
     if not os.path.exists(file_name):
         with open(file_name, mode='w', newline='') as file:
             writer=csv.writer(file)
             writer.writerow(["Date", "Category", "Amount", "Description"])
     try:
-        date = datetime.strptime(input("Enter date (YYYY-MM-DD): "), "%Y-%m-%d")
+        date = datetime.strptime(input("Enter date (YYYY-MM-DD): "), "%Y-%m-%d").date()
         category=input("Enter the category : ")
         amount=float(input("Enter the expense : "))
         description=input("Enter description og ")
@@ -30,8 +28,29 @@ def add_expense():
         print("Enter valid details.")
     return 
 
+def view_expenses():
+    if not os.path.exists(file_name):
+        print("No expenses found.")
+    with open(file_name, mode='r') as file:
+        reader=csv.reader(file)
+        rows=list(reader)
+
+        if len(rows)<=1:
+            print("No enpenses added.")
+            return 
+    print("\n...All expenses...")
+    print("S.no \tDate \tCategory \tAamount \tDescription")
+    for i in range(1, len(rows)):
+        single_row = rows[i]
+
+        date = single_row[0]
+        category = single_row[1]
+        amount = single_row[2]
+        description = single_row[3]
+
+        print(i,"\t", date,"\t", category,"\t", amount,"\t", description)
 add_expense()
-    
+view_expenses()
 
 
 
